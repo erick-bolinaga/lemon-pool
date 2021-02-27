9569010b8db5293fa086b44cff62bf34a8c0e206
erick_bolinaga <habanero01@gmail.com>
2021-02-27 21:59:38 +0300

    pool illustration, ada price

--- js/pool.js
+++ js/pool.js
@@ -1,5 +1,4 @@
 $.getJSON('https://js.adapools.org/pools/2dd26a3b9dd2372c8c483c730d2a47a6f51e51c06b4a9c248f6dd3ac/summary.json', function(data) {
-	console.log(data);
 	$.each( data.data, function( i, val ) { 
 		a = new Array('tax_fix','pledge','total_stake');
 		if (a.includes(i)) val = Math.round(parseInt(val) / 1000000);
@@ -27,4 +26,8 @@ $.getJSON('https://js.adapools.org/pools/2dd26a3b9dd2372c8c483c730d2a47a6f51e51c
 				$('#2dd26a3b9dd2372c8c483c730d2a47a6f51e51c06b4a9c248f6dd3ac_'+i).html(val).text();
 		}
 	}); 
+});
+$.getJSON('https://api.coingecko.com/api/v3/simple/price?ids=cardano&vs_currencies=USD', function(data) {
+	console.log(data.cardano.usd);
+	$('#ada-price').html('$ ' + data.cardano.usd);
 });
