587cfa1275230fa6f38a4d4b79bf53829acaeed1
erick_bolinaga <habanero01@gmail.com>
2021-04-10 21:31:26 +0300

    feat: faq

--- js/main.js
+++ js/main.js
@@ -57,6 +57,21 @@ $(document).ready(function() {
   });
 });
 
+let acc = document.getElementsByClassName("faq-question");
+let i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
+
 /* Morphing button */
 (function() { 
   var docElem = window.document.documentElement, didScroll, scrollPosition;
